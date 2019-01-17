e34d3e77cb2b87c3a142058f72b14518e9b3eaf9
Kristen <kristen.fyffe@gmail.com>
2019-01-17 02:15:42 +0000

    added downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var gitToken = require('./secrets');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -16,11 +17,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
     var repos = JSON.parse(body);
     var result = [];
     for (var i = 0; i < repos.length; i++){
-      result.push(repos[i].avatar_url);
+      result.push({
+        avatar_url: repos[i].avatar_url,
+        name: repos[i].login
+      });
+
     }
     cb(err, result);
   })
 }
+
+function downloadImageByURL(url, filePath) {
+  console.log(678, url, filePath);
+  request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .on('data', function (response){
+      console.log('Downloading image...');
+    })
+    .on('end', function (response){
+      console.log('Download complete.');
+    })
+    .pipe(fs.createWriteStream(filePath));
+      console.log()
+}
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
